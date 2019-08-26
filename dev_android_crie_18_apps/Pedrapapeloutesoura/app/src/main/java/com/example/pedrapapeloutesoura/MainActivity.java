cbca2813420454cf055038a77ccbf4b99e9a998d
rafaelrsanches <rafael.rsanches96@gmail.com>
2019-08-26 20:59:39 -0300

    Escolhendo opção #App3 Pedra, papel ou tesoura - Parte 02

--- dev_android_crie_18_apps/Pedrapapeloutesoura/app/src/main/java/com/example/pedrapapeloutesoura/MainActivity.java
+++ dev_android_crie_18_apps/Pedrapapeloutesoura/app/src/main/java/com/example/pedrapapeloutesoura/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +29,23 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void opcaoSelecionada(String escolhaUsuario){
-        System.out.println(escolhaUsuario);
+        ImageView imagemResultado = findViewById(R.id.imageResultado);
+
+        // gerar número aleatório para escolha do App
+        String[] opcoes = {"pedra", "papel", "tesoura"};
+        int numero = new Random().nextInt(3);
+        String escolhaApp = opcoes[numero];
+
+        switch (escolhaApp){
+            case "pedra":
+                imagemResultado.setImageResource(R.drawable.pedra);
+                break;
+            case "papel":
+                imagemResultado.setImageResource(R.drawable.papel);
+                break;
+            case "tesoura":
+                imagemResultado.setImageResource(R.drawable.tesoura);
+                break;
+        }
     }
 }
