71b9ea20dbba536fe45a63ac66f585157bbf4164
rafaelrsanches <rafael.rsanches96@gmail.com>
2019-08-26 20:01:06 -0300

    Escolhendo opção #App3 Pedra, papel ou tesoura - Parte 01

--- dev_android_crie_18_apps/Pedrapapeloutesoura/app/src/main/java/com/example/pedrapapeloutesoura/MainActivity.java
+++ dev_android_crie_18_apps/Pedrapapeloutesoura/app/src/main/java/com/example/pedrapapeloutesoura/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.pedrapapeloutesoura;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void selecionarPedra(View view){
+        this.opcaoSelecionada("pedra");
+    }
+
+    public void selecionarPapel(View view){
+        this.opcaoSelecionada("papel");
+    }
+
+    public void selecionarTesoura(View view){
+        this.opcaoSelecionada("tesoura");
+    }
+
+    public void opcaoSelecionada(String escolhaUsuario){
+        System.out.println(escolhaUsuario);
     }
 }
