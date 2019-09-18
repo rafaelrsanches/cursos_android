0868fe096c4fe2633782c124451f10dc3f9da9d9
rafaelrsanches <rafael.rsanches96@gmail.com>
2019-09-17 21:46:32 -0300

    Check Box

--- dev_android_crie_18_apps/ComponentesBasicos/app/src/main/java/com/example/componentesbasicos/MainActivity.java
+++ dev_android_crie_18_apps/ComponentesBasicos/app/src/main/java/com/example/componentesbasicos/MainActivity.java
@@ -4,14 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextInputEditText nomeProduto;
     private TextView tvResultado;
+    private CheckBox cbBranco,
+                     cbVerde,
+                     cbVermelho;
+    List<String> check = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,12 +28,34 @@ public class MainActivity extends AppCompatActivity {
 
         nomeProduto = findViewById(R.id.etNomeProduto);
         tvResultado = findViewById(R.id.tvResultado);
+        cbBranco = findViewById(R.id.cbBranco);
+        cbVerde = findViewById(R.id.cbVerde);
+        cbVermelho = findViewById(R.id.cbVermelho);
+    }
+
+    public void verificaCheck(){
+        check.clear();
+
+        if(cbBranco.isChecked()){
+            check.add(cbBranco.getText().toString());
+        }
+        if(cbVerde.isChecked()){
+            check.add(cbVerde.getText().toString());
+        }
+        if(cbVermelho.isChecked()){
+            check.add(cbVermelho.getText().toString());
+        }
+
+        tvResultado.setText(check.toString());
     }
 
     public void btnEnviar(View view){
+        /*
         String produto = nomeProduto.getText().toString();
-
         tvResultado.setText(produto);
+         */
+
+        verificaCheck();
     }
 
 }
