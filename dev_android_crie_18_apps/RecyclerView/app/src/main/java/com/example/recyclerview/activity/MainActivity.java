7ca3321f334cdae56ba3abbce0a5b03d950fef00
rafaelrsanches <rafael.rsanches96@gmail.com>
2019-09-24 20:23:35 -0300

    RecyclerView - Parte 01

---
+++ dev_android_crie_18_apps/RecyclerView/app/src/main/java/com/example/recyclerview/activity/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.recyclerview.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import com.example.recyclerview.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+
+        // Configurar Adapter
+
+
+        //Configurar RecyclerView
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true);
+        //recyclerView.setAdapter();
+    }
+}
