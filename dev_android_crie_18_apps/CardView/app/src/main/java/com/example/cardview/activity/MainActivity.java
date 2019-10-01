63b37d5209c72d766d975f126ce66f6b8abbb79d
rafaelrsanches <rafael.rsanches96@gmail.com>
2019-10-01 20:01:32 -0300

    CardView - Parte 01

---
+++ dev_android_crie_18_apps/CardView/app/src/main/java/com/example/cardview/activity/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.cardview.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import com.example.cardview.R;
+import com.example.cardview.adapter.PostagemAdapter;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerPostagem;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerPostagem = findViewById(R.id.recyclerPostagem);
+
+        // Define layout
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerPostagem.setLayoutManager(layoutManager);
+
+        // Define adapter
+        PostagemAdapter adapter = new PostagemAdapter();
+        recyclerPostagem.setAdapter(adapter);
+    }
+}
---
+++ dev_android_crie_18_apps/CardView/app/src/main/java/com/example/cardview/adapter/PostagemAdapter.java
@@ -0,0 +1,39 @@
+package com.example.cardview.adapter;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class PostagemAdapter extends RecyclerView.Adapter<PostagemAdapter.MyViewHolder> {
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+        private TextView nome;
+        private TextView postagem;
+        private ImageView imagem;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
