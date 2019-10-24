3ffac76c9b918fa76f86033cc3e2ef729d73e4a2
rafaelrsanches <rafael.rsanches96@gmail.com>
2019-10-24 20:56:35 -0300

    Executando sons

---
+++ dev_android_crie_18_apps/MediaPlayer/app/src/main/java/com/example/mediaplayer/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.mediaplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MediaPlayer mediaPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.teste);
+    }
+
+    public void executarSom(View view){
+        if (mediaPlayer != null){
+            mediaPlayer.start();
+        }
+    }
+}
