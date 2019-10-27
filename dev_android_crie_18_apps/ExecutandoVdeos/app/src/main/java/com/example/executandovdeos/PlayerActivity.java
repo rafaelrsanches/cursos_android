052fa42dea70b40ad31d38337c20620110fc39ad
rafaelrsanches <rafael.rsanches96@gmail.com>
2019-10-27 13:06:19 -0300

    Executando Vídeos - Parte 02

---
+++ dev_android_crie_18_apps/ExecutandoVdeos/app/src/main/java/com/example/executandovdeos/PlayerActivity.java
@@ -0,0 +1,34 @@
+package com.example.executandovdeos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class PlayerActivity extends AppCompatActivity {
+
+    private VideoView videoView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player);
+
+        videoView = findViewById(R.id.videoView);
+
+        // Esconde a ActionBar
+        getSupportActionBar().hide();
+
+        // Esconde a StatusBar e a NavigationBar
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+
+        // Executar o vídeo
+        videoView.setMediaController(new MediaController(this));
+        videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.video);
+        videoView.start();
+    }
+}
