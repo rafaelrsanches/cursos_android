454a66c784c04f774443367bedd84951c7aa91f0
rafaelrsanches <rafael.rsanches96@gmail.com>
2019-10-27 22:44:11 -0300

    Criando interface de Abas #App8 - Aprenda Inglês

---
+++ dev_android_crie_18_apps/AprendaIngles/app/src/main/java/com/example/aprendaingles/activities/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.aprendaingles.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.example.aprendaingles.R;
+import com.example.aprendaingles.fragments.AnimaisFragment;
+import com.example.aprendaingles.fragments.NumerosFragment;
+import com.example.aprendaingles.fragments.VogaisFragment;
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItem;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SmartTabLayout smartTabLayout;
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        smartTabLayout = findViewById(R.id.smartTabLayout);
+        viewPager = findViewById(R.id.viewPager);
+
+        // Configura ActionBar
+        getSupportActionBar().setElevation(0);
+
+        FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter(
+                getSupportFragmentManager(),
+                FragmentPagerItems.with(this)
+                .add("Animais", AnimaisFragment.class)
+                .add("Números", NumerosFragment.class)
+                .add("Vogais", VogaisFragment.class)
+                .create()
+        );
+
+        viewPager.setAdapter(adapter);
+        smartTabLayout.setViewPager(viewPager);
+    }
+}
