3a4e4b160f637a62fc6a0f3d9519a7a7c16b1484
rafaelrsanches <rafael.rsanches96@gmail.com>
2020-01-13 21:22:57 -0300

    Adicionando menu

--- dev_android_crie_18_apps/Listadetarefas/app/src/main/java/com/example/listadetarefas/activity/AdicionarTarefaActivity.java
+++ dev_android_crie_18_apps/Listadetarefas/app/src/main/java/com/example/listadetarefas/activity/AdicionarTarefaActivity.java
@@ -1,8 +1,12 @@
 package com.example.listadetarefas.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.listadetarefas.R;
 
@@ -12,5 +16,27 @@ public class AdicionarTarefaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adicionar_tarefa);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_adicionar_tarefa, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.itemSalvar:
+                Toast.makeText(AdicionarTarefaActivity.this,
+                        "Botão adicionar tarefa pressionado",
+                        Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
