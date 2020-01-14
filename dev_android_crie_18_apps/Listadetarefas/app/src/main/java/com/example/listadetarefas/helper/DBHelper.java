e45866c61f81aece1a26bf0dc406fa9c21d10597
rafaelrsanches <rafael.rsanches96@gmail.com>
2020-01-14 20:33:02 -0300

    Configurando banco de dados - Parte 02

--- dev_android_crie_18_apps/Listadetarefas/app/src/main/java/com/example/listadetarefas/helper/DBHelper.java
+++ dev_android_crie_18_apps/Listadetarefas/app/src/main/java/com/example/listadetarefas/helper/DBHelper.java
@@ -3,6 +3,7 @@ package com.example.listadetarefas.helper;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
 
@@ -21,12 +22,30 @@ public class DBHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
 
         String sql = "CREATE TABLE IF NOT EXISTS " + TABELA_TAREFAS
-                    + " (id INTEGER PRIMARY KEY)";
+                    + " (id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    " nome TEXT NOT NULL); ";
+
+        try{
+            sqLiteDatabase.execSQL(sql);
+            Log.i("INFO DB", "Sucesso ao criar tabela");
+        }catch (Exception e){
+            Log.i("INFO DB", "Erro ao criar tabela" + e.getMessage());
+        }
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+        String sql = "DROP TABLE IF EXISTS " + TABELA_TAREFAS + " ;";
+
+        try{
+            sqLiteDatabase.execSQL(sql);
+            onCreate(sqLiteDatabase);
+            Log.i("INFO DB", "Sucesso ao atualizar App");
+        }catch (Exception e){
+            Log.i("INFO DB", "Erro ao atualizar App" + e.getMessage());
+        }
 
     }
 }
