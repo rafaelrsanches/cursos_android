24effc4218f4e2fdb4e8b2b044e0f6ed55230e1f
rafaelrsanches <rafael.rsanches96@gmail.com>
2020-01-21 21:30:01 -0300

    Salvando e atualizando dados

--- dev_android_crie_18_apps/FirebaseApp/app/src/main/java/com/example/firebaseapp/MainActivity.java
+++ dev_android_crie_18_apps/FirebaseApp/app/src/main/java/com/example/firebaseapp/MainActivity.java
@@ -4,11 +4,35 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
+
+    private DatabaseReference referencia = FirebaseDatabase.getInstance().getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+         DatabaseReference usuarios = referencia.child("usuarios");
+         DatabaseReference produtos = referencia.child("produtos");
+
+         Usuario usuario = new Usuario();
+         usuario.setNome("Maria");
+         usuario.setSobrenome("Silva");
+         usuario.setIdade(58);
+
+         usuarios.child("002").setValue(usuario);
+
+
+         Produto produto = new Produto();
+         produto.setDescricao("330s");
+         produto.setMarca("Lenovo");
+         produto.setPreco(3299.90);
+
+         produtos.child("002").setValue(produto);
+
     }
 }
