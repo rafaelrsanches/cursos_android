6fefbf3780b4382c6a94a3b633333f886ea80ff1
rafaelrsanches <rafael.rsanches96@gmail.com>
2020-02-10 23:54:26 -0300

    Utilizando Slider

--- dev_android_crie_18_apps/Organizze/app/src/main/java/com/example/organizze/MainActivity.java
+++ dev_android_crie_18_apps/Organizze/app/src/main/java/com/example/organizze/MainActivity.java
@@ -4,11 +4,63 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.heinrichreimersoftware.materialintro.app.IntroActivity;
+import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
+import com.heinrichreimersoftware.materialintro.slide.SimpleSlide;
+
+public class MainActivity extends IntroActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+
+
+        setButtonBackVisible(false);
+        setButtonNextVisible(false);
+
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_1)
+                .build()
+        );
+
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_2)
+                .build()
+        );
+
+        /*
+        setButtonBackVisible(false);
+        setButtonNextVisible(false);
+
+        addSlide(new SimpleSlide.Builder()
+                .title("Titulo")
+                .description("Descrição")
+                .image(R.mipmap.um)
+                .background(android.R.color.holo_orange_light)
+                .build()
+        );
+
+        addSlide(new SimpleSlide.Builder()
+                .title("Titulo2")
+                .description("Descrição2")
+                .image(R.mipmap.dois)
+                .background(android.R.color.holo_orange_light)
+                .build()
+        );
+
+        addSlide(new SimpleSlide.Builder()
+                .title("Titulo3")
+                .description("Descrição3")
+                .image(R.mipmap.tres)
+                .background(android.R.color.holo_orange_light)
+                .build()
+        );
+         */
+
+
+
     }
 }
