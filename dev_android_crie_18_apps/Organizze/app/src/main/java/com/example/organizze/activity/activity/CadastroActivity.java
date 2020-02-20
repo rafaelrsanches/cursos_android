907295ff79354cc21bf04909532564aec3c58aa9
rafaelrsanches <rafael.rsanches96@gmail.com>
2020-02-20 19:07:18 -0300

    Tratamento de exceções de cadastro

--- dev_android_crie_18_apps/Organizze/app/src/main/java/com/example/organizze/activity/activity/CadastroActivity.java
+++ dev_android_crie_18_apps/Organizze/app/src/main/java/com/example/organizze/activity/activity/CadastroActivity.java
@@ -16,6 +16,9 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
 
 public class CadastroActivity extends AppCompatActivity {
 
@@ -87,8 +90,23 @@ public class CadastroActivity extends AppCompatActivity {
                             "Sucesso ao cadastrar usuário!",
                             Toast.LENGTH_SHORT).show();
                 }else{
+
+                    String excecao = "";
+                    try {
+                        throw task.getException();
+                    }catch (FirebaseAuthWeakPasswordException e){
+                        excecao = "Digite uma senha mais forte!";
+                    }catch (FirebaseAuthInvalidCredentialsException e){
+                        excecao = "Digite um e-mail válido!";
+                    }catch (FirebaseAuthUserCollisionException e){
+                        excecao = "E-mail já existente";
+                    }catch (Exception e){
+                        excecao = "Erro ao cadastrar usuário" + e.getMessage();
+                        e.printStackTrace();
+                    }
+
                     Toast.makeText(CadastroActivity.this,
-                            "Erro ao cadastrar usuário!",
+                            excecao,
                             Toast.LENGTH_SHORT).show();
                 }
             }
