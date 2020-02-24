a01d40d0eb03a929e1e41a7000f0ab40074a9f47
rafaelrsanches <rafael.rsanches96@gmail.com>
2020-02-24 19:14:21 -0300

    Validar e autenticar usuário - Parte 01

--- dev_android_crie_18_apps/Organizze/app/src/main/java/com/example/organizze/activity/activity/LoginActivity.java
+++ dev_android_crie_18_apps/Organizze/app/src/main/java/com/example/organizze/activity/activity/LoginActivity.java
@@ -1,16 +1,112 @@
 package com.example.organizze.activity.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.organizze.R;
+import com.example.organizze.activity.config.ConfiguracaoFirebase;
+import com.example.organizze.activity.model.Usuario;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText editEmail, editSenha;
+    private Button buttonEntrar;
+    private Usuario usuario;
+    private FirebaseAuth autenticacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        editEmail = findViewById(R.id.editEmail);
+        editSenha = findViewById(R.id.editSenha);
+        buttonEntrar = findViewById(R.id.buttonEntrar);
+
+        buttonEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String textoEmail = editEmail.getText().toString();
+                String textoSenha = editSenha.getText().toString();
+
+                if (!textoEmail.isEmpty()){
+                    if (!textoSenha.isEmpty()){
+
+                        usuario = new Usuario();
+                        usuario.setEmail(textoEmail);
+                        usuario.setSenha(textoSenha);
+                        validarLogin();
+
+                    }else{
+                        Toast.makeText(LoginActivity.this,
+                                "Preencha a senha!",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                }else{
+                    Toast.makeText(LoginActivity.this,
+                            "Preencha o email!",
+                            Toast.LENGTH_SHORT).show();
+                }
+
+
+            }
+        });
+
     }
+
+    public void  validarLogin(){
+
+        autenticacao = ConfiguracaoFirebase.getFirebaseAuth();
+        autenticacao.signInWithEmailAndPassword(
+                usuario.getEmail(),
+                usuario.getSenha()
+        ).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+
+                    Toast.makeText(LoginActivity.this,
+                            "Sucesso ao fazer login",
+                            Toast.LENGTH_LONG).show();
+
+                }else {
+
+                    String excecao = "";
+                    try {
+                        throw task.getException();
+                    }catch (FirebaseAuthInvalidUserException e){
+                        excecao = "Usuário não está cadastrado!";
+                    }catch (FirebaseAuthInvalidCredentialsException e){
+                        excecao = "E-mail ou senha incorretos!";
+                    }catch (Exception e){
+                        excecao = "Erro ao cadastrar usuário" + e.getMessage();
+                        e.printStackTrace();
+                    }
+
+                    Toast.makeText(LoginActivity.this,
+                            excecao,
+                            Toast.LENGTH_SHORT).show();
+
+                }
+                
+            }
+        });
+
+    }
+
 }
