2400905c0a1b5f7667fa94e824bb9c5054eab21b
rafaelrsanches <rafael.rsanches96@gmail.com>
2020-05-02 00:48:36 -0300

    Configurando Toolbar

--- dev_android_crie_18_apps/Organizze/app/src/main/java/com/example/organizze/activity/activity/PrincipalActivity.java
+++ dev_android_crie_18_apps/Organizze/app/src/main/java/com/example/organizze/activity/activity/PrincipalActivity.java
@@ -3,16 +3,21 @@ package com.example.organizze.activity.activity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.organizze.activity.config.ConfiguracaoFirebase;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
 import com.example.organizze.R;
+import com.google.firebase.auth.FirebaseAuth;
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 import com.prolificinteractive.materialcalendarview.OnMonthChangedListener;
@@ -21,13 +26,16 @@ public class PrincipalActivity extends AppCompatActivity {
 
     private MaterialCalendarView materialCalendarView;
     private TextView textSaudacao, textSaldo;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
         Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("Organizze");
         setSupportActionBar(toolbar);
+
 
         textSaudacao = findViewById(R.id.textSaudacao);
         textSaldo = findViewById(R.id.textSaldo);
@@ -45,6 +53,25 @@ public class PrincipalActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });*/
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_principal, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menuSair:
+                auth = ConfiguracaoFirebase.getFirebaseAuth();
+                auth.signOut();
+                startActivity(new Intent(this, MainActivity.class));
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public void adicionarReceita(View view){
