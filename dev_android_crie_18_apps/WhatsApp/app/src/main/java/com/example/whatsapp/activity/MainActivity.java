bca3d9de4bf60aecbd2a0e86e3e2e83d1e957b7a
rafaelrsanches <rafael.rsanches96@gmail.com>
2020-05-19 23:18:07 -0300

    Adicionando menus

--- dev_android_crie_18_apps/WhatsApp/app/src/main/java/com/example/whatsapp/activity/MainActivity.java
+++ dev_android_crie_18_apps/WhatsApp/app/src/main/java/com/example/whatsapp/activity/MainActivity.java
@@ -1,22 +1,66 @@
 package com.example.whatsapp.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.example.whatsapp.R;
+import com.example.whatsapp.config.ConfiguracaoFirebase;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        auth = ConfiguracaoFirebase.getFirebaseAuth();
+
         Toolbar toolbar = findViewById(R.id.toolbarPrincipal);
         toolbar.setTitle("WhatsApp");
         setSupportActionBar(toolbar);
 
     }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.menuSair:
+                deslogarUsuario();
+                finish();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    public void deslogarUsuario(){
+        try {
+            auth.signOut();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
 }
