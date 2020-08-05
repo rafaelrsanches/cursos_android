b5c10b0f0dd8792bdcb42fd1decb674eed8e98d8
rafaelrsanches <rafael.rsanches96@gmail.com>
2020-08-04 23:39:06 -0300

    Interface de configuração & imagem arredondada - Parte 01

--- dev_android_crie_18_apps/WhatsApp/app/src/main/java/com/example/whatsapp/activity/MainActivity.java
+++ dev_android_crie_18_apps/WhatsApp/app/src/main/java/com/example/whatsapp/activity/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -69,6 +70,9 @@ public class MainActivity extends AppCompatActivity {
                 deslogarUsuario();
                 finish();
                 break;
+            case R.id.menuConfiguracoes:
+                abrirConfiguracoes();
+                break;
         }
 
         return super.onOptionsItemSelected(item);
@@ -81,6 +85,11 @@ public class MainActivity extends AppCompatActivity {
         }catch (Exception e){
             e.printStackTrace();
         }
+    }
+
+    public void abrirConfiguracoes(){
+        Intent intent = new Intent(MainActivity.this, ConfiguracoesActivity.class);
+        startActivity(intent);
     }
 
 }
