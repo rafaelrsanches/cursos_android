0c721e23dfd37088fb114141138496fe5d2b7659
rafaelrsanches <rafael.rsanches96@gmail.com>
2020-09-13 15:32:10 -0300

    Permissões no Android Marshmallow - Parte 02

--- dev_android_crie_18_apps/WhatsApp/app/src/main/java/com/example/whatsapp/activity/ConfiguracoesActivity.java
+++ dev_android_crie_18_apps/WhatsApp/app/src/main/java/com/example/whatsapp/activity/ConfiguracoesActivity.java
@@ -1,9 +1,13 @@
 package com.example.whatsapp.activity;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import com.example.whatsapp.R;
@@ -22,12 +26,46 @@ public class ConfiguracoesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_configuracoes);
 
         // Validar permissões
-        Permissao.validarPermissoes(permissoesNecessarias, this);
+        Permissao.validarPermissoes(permissoesNecessarias, this, 1);
 
         Toolbar toolbar = findViewById(R.id.toolbarPrincipal);
         toolbar.setTitle("Configurações");
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        for (int permissaoResultado : grantResults){
+
+            if (permissaoResultado == PackageManager.PERMISSION_DENIED){
+
+                alertaValidacaoPermissao();
+
+            }
+
+        }
+
+    }
+
+    private void alertaValidacaoPermissao(){
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Permissões Negadas");
+        builder.setMessage("Para utilizar o App é necessário aceitar as permissões");
+        builder.setCancelable(false);
+        builder.setPositiveButton("Confirmar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                finish();
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
     }
 }
--- dev_android_crie_18_apps/WhatsApp/app/src/main/java/com/example/whatsapp/helper/Permissao.java
+++ dev_android_crie_18_apps/WhatsApp/app/src/main/java/com/example/whatsapp/helper/Permissao.java
@@ -1,8 +1,10 @@
 package com.example.whatsapp.helper;
 
 import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Build;
 
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import java.util.ArrayList;
@@ -10,21 +12,35 @@ import java.util.List;
 
 public class Permissao {
 
-    public static boolean validarPermissoes(String[] permissoes, Activity activity){
+    public static boolean validarPermissoes(String[] permissoes, Activity activity, int requestCode) {
 
-        if (Build.VERSION.SDK_INT >= 23){
+        if (Build.VERSION.SDK_INT >= 23) {
 
             List<String> listaPermissoes = new ArrayList<>();
 
             // Percorrer as permissões passadas, verificando
             //  uma a uma se já tem a permissão liberada.
-            for (String permissao: permissoes){
+            for (String permissao : permissoes) {
 
-                ContextCompat.checkSelfPermission(activity, permissao);
+                Boolean temPermissao = ContextCompat.checkSelfPermission(activity, permissao) == PackageManager.PERMISSION_GRANTED;
+
+                if (!temPermissao) listaPermissoes.add(permissao);
+
+            }
+
+            // Caso a lista esteja vazia, não é necessário solicitar permissão
+            if (listaPermissoes.isEmpty()) return true;
+
+            // Converte a lista de String em um Array de Strings
+            String[] novasPermissoes = new String[listaPermissoes.size()];
+            listaPermissoes.toArray(novasPermissoes);
+
+            // Solicita permissão
+            ActivityCompat.requestPermissions(activity, novasPermissoes, requestCode);
 
         }
 
         return true;
-    }
 
+    }
 }
