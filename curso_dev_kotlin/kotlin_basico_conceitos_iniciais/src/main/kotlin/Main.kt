9fc6d94cc1bbcebbf20130713f6f3b735d8f8a4e
Rafael R. Sanches <rafael.rsanches96@gmail.com>
2022-08-18 16:35:18 -0300

    Variáveis mutáveis e imutáveis

--- curso_dev_kotlin/kotlin_basico_conceitos_iniciais/src/main/kotlin/Main.kt
+++ curso_dev_kotlin/kotlin_basico_conceitos_iniciais/src/main/kotlin/Main.kt
@@ -1,13 +1,6 @@
 fun main() {
-    // Unsigned
+    var nome: String = "Tony"
+    nome = "Stark"
 
-    println("ULong MIN ${ULong.MIN_VALUE} - MAX ${ULong.MAX_VALUE}")
-    println("UInt MIN ${UInt.MIN_VALUE} - MAX ${UInt.MAX_VALUE}")
-    println("UShort MIN ${UShort.MIN_VALUE} - MAX ${UShort.MAX_VALUE}")
-    println("UByte MIN ${UByte.MIN_VALUE} - MAX ${UByte.MAX_VALUE}")
-
-    val abc: UInt = 0u
-    val short: UShort = 10u
-    val byte: UByte = 10u
-    val long: ULong = 10u
+    val pseudo: String = "Homem de Ferro"
 }
