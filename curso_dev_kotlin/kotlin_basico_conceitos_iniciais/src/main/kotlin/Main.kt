cdb34a3d947856c2df4f2a914b9963f203ef21f4
Rafael R. Sanches <rafael.rsanches96@gmail.com>
2022-08-18 16:20:52 -0300

    Tipos de dados - Unsigned

--- curso_dev_kotlin/kotlin_basico_conceitos_iniciais/src/main/kotlin/Main.kt
+++ curso_dev_kotlin/kotlin_basico_conceitos_iniciais/src/main/kotlin/Main.kt
@@ -1,22 +1,13 @@
 fun main() {
+    // Unsigned
 
-    var c: Char = 'a'
-    var s: String = "palavra ou palavras"
-    var b: Boolean = true
+    println("ULong MIN ${ULong.MIN_VALUE} - MAX ${ULong.MAX_VALUE}")
+    println("UInt MIN ${UInt.MIN_VALUE} - MAX ${UInt.MAX_VALUE}")
+    println("UShort MIN ${UShort.MIN_VALUE} - MAX ${UShort.MAX_VALUE}")
+    println("UByte MIN ${UByte.MIN_VALUE} - MAX ${UByte.MAX_VALUE}")
 
-    var double: Double = 10.0
-    var float: Float = 10f
-    var long: Long = 10L
-    var int: Int = 10
-    var short: Short = 10
-    var byte: Byte = 10
-
-    var idade: Byte = 40
-
-    println("Double MIN ${Double.MIN_VALUE} - MAX ${Double.MAX_VALUE}")
-    println("Float MIN ${Float.MIN_VALUE} - MAX ${Float.MAX_VALUE}")
-    println("Long MIN ${Long.MIN_VALUE} - MAX ${Long.MAX_VALUE}")
-    println("Int MIN ${Int.MIN_VALUE} - MAX ${Int.MAX_VALUE}")
-    println("Short MIN ${Short.MIN_VALUE} - MAX ${Short.MAX_VALUE}")
-    println("Byte MIN ${Byte.MIN_VALUE} - MAX ${Byte.MAX_VALUE}")
+    val abc: UInt = 0u
+    val short: UShort = 10u
+    val byte: UByte = 10u
+    val long: ULong = 10u
 }
