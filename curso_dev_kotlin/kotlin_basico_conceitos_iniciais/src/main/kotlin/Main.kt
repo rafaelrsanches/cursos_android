f2dd0caaee049b2e3c6661540bce3be6c06ade04
Rafael R. Sanches <rafael.rsanches96@gmail.com>
2022-08-22 09:30:36 -0300

    Funções matemáticas (Math)

--- curso_dev_kotlin/kotlin_basico_conceitos_iniciais/src/main/kotlin/Main.kt
+++ curso_dev_kotlin/kotlin_basico_conceitos_iniciais/src/main/kotlin/Main.kt
@@ -1,23 +1,10 @@
+import kotlin.math.*
+
 fun main() {
-    val str = "Programação Kotlin"
-
-    println("Tamanho da string: ${str.length}")
-
-    println("Posição 0 da string: ${str[0]}")
-    println("Posição 1 da string: ${str[1]}")
-    println("Posição 2 da string: ${str[2]}")
-    println("Posição 3 da string: ${str[3]}")
-
-    println(str.startsWith("Pro", true))
-    println(str.endsWith("lin", true))
-
-    println(str.substring(2, 5))
-
-    println(str.replace("Programação", "Dev", true))
-
-    println(str.lowercase())
-
-    println(str.uppercase())
-
-    println(str.trim()) // Elimina espaços em branco nas extremidades da String
+    println(max(5, 10))
+    println(min(5, 10))
+    println(sqrt(45.67f))
+    println(PI)
+    println(E)
+    println(round(4536.36536))
 }
