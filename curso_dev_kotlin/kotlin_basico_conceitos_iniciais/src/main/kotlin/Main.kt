bafb5e871911c64744b401f4c234f94beeae6a24
Rafael R. Sanches <rafael.rsanches96@gmail.com>
2022-08-22 11:09:23 -0300

    Resolução - Funções

--- curso_dev_kotlin/kotlin_basico_conceitos_iniciais/src/main/kotlin/Main.kt
+++ curso_dev_kotlin/kotlin_basico_conceitos_iniciais/src/main/kotlin/Main.kt
@@ -1,10 +1,26 @@
 import kotlin.math.*
 
 fun main() {
-    println(max(5, 10))
-    println(min(5, 10))
-    println(sqrt(45.67f))
-    println(PI)
-    println(E)
-    println(round(4536.36536))
+    converteAnos(2)
+    qntdChar("Quantidade de caracter")
+    println(calculaCuboInt(3))
+    println(milhasEmKm(5f))
+    trocaChar("Troca A por X")
 }
+
+fun converteAnos(anos: Int) {
+    println("$anos anos equivalem a: ")
+    println("${(anos * 12)} meses")
+    println("${(anos * 365)} dias")
+    println("${(anos * 365 * 24)} horas")
+    println("${(anos * 365 * 24 * 60)} minutos")
+    println("${(anos * 365 * 24 * 60 * 60)} segundos")
+}
+
+fun qntdChar(str: String) = println(str.length)
+
+fun calculaCuboInt(n: Int): Int = (n * n * n)
+
+fun milhasEmKm (n: Float): Float = (n * 1.6f)
+
+fun trocaChar(str: String) = println(str.replace("a", "x", true).lowercase())
