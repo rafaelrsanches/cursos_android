af0072fde7bf94e760f179d9a63ca83eed5b76a2
Rafael R. Sanches <rafael.rsanches96@gmail.com>
2022-08-22 08:48:28 -0300

    Funções de String

--- curso_dev_kotlin/kotlin_basico_conceitos_iniciais/src/main/kotlin/Main.kt
+++ curso_dev_kotlin/kotlin_basico_conceitos_iniciais/src/main/kotlin/Main.kt
@@ -1,12 +1,23 @@
 fun main() {
-    helloWorld()
-    println("A soma de 10 e 5 é: ${sum(10, 5)}")
+    val str = "Programação Kotlin"
 
-    println(divisao(10f, 3.5f))
+    println("Tamanho da string: ${str.length}")
+
+    println("Posição 0 da string: ${str[0]}")
+    println("Posição 1 da string: ${str[1]}")
+    println("Posição 2 da string: ${str[2]}")
+    println("Posição 3 da string: ${str[3]}")
+
+    println(str.startsWith("Pro", true))
+    println(str.endsWith("lin", true))
+
+    println(str.substring(2, 5))
+
+    println(str.replace("Programação", "Dev", true))
+
+    println(str.lowercase())
+
+    println(str.uppercase())
+
+    println(str.trim()) // Elimina espaços em branco nas extremidades da String
 }
-
-fun sum(a: Int, b: Int): Int = (a + b)
-
-fun helloWorld() = println("Hello, world!")
-
-fun divisao(a: Float, b: Float): Float = (a / b)
