dc4418cd282244f87db35087069d811ccf9a5e94
Rafael R. Sanches <rafael.rsanches96@gmail.com>
2022-08-22 08:35:17 -0300

    Funções em uma única linha

--- curso_dev_kotlin/kotlin_basico_conceitos_iniciais/src/main/kotlin/Main.kt
+++ curso_dev_kotlin/kotlin_basico_conceitos_iniciais/src/main/kotlin/Main.kt
@@ -1,12 +1,12 @@
 fun main() {
     helloWorld()
     println("A soma de 10 e 5 é: ${sum(10, 5)}")
+
+    println(divisao(10f, 3.5f))
 }
 
-fun sum(a: Int, b: Int): Int{
-    return (a + b)
-}
+fun sum(a: Int, b: Int): Int = (a + b)
 
-fun helloWorld(){
-    println("Hello, world!")
-}
+fun helloWorld() = println("Hello, world!")
+
+fun divisao(a: Float, b: Float): Float = (a / b)
