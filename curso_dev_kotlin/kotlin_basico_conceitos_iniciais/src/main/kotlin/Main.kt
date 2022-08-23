f1ab9d9b35f34e9e17f811a31dd257ca348ecc31
Rafael R. Sanches <rafael.rsanches96@gmail.com>
2022-08-23 14:56:07 -0300

    Controle de fluxo if else

--- curso_dev_kotlin/kotlin_basico_conceitos_iniciais/src/main/kotlin/Main.kt
+++ curso_dev_kotlin/kotlin_basico_conceitos_iniciais/src/main/kotlin/Main.kt
@@ -1,26 +1,29 @@
 import kotlin.math.*
 
 fun main() {
-    converteAnos(2)
-    qntdChar("Quantidade de caracter")
-    println(calculaCuboInt(3))
-    println(milhasEmKm(5f))
-    trocaChar("Troca A por X")
+    maiorDeIdade(19)
+    maiorDeIdade(15)
 }
 
-fun converteAnos(anos: Int) {
-    println("$anos anos equivalem a: ")
-    println("${(anos * 12)} meses")
-    println("${(anos * 365)} dias")
-    println("${(anos * 365 * 24)} horas")
-    println("${(anos * 365 * 24 * 60)} minutos")
-    println("${(anos * 365 * 24 * 60 * 60)} segundos")
+fun maiorDeIdade(idade: Int) {
+    if (idade >= 18){
+        println("Maior de idade")
+
+        if (idade > 60){
+            print("Terceira idade")
+        }
+
+    } else if (idade < 10) {
+        println("Criança")
+    } else {
+        print("Menor de idade")
+    }
 }
 
-fun qntdChar(str: String) = println(str.length)
-
-fun calculaCuboInt(n: Int): Int = (n * n * n)
-
-fun milhasEmKm (n: Float): Float = (n * 1.6f)
-
-fun trocaChar(str: String) = println(str.replace("a", "x", true).lowercase())
+fun saudacao(dia: Boolean): String {
+    return if (dia){
+        "Bom dia"
+    } else {
+        "Boa noite"
+    }
+}
