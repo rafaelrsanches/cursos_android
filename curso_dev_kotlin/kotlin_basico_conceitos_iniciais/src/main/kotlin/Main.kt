4af6bd78740f3470d0207c208fb015a5565f7519
Rafael R. Sanches <rafael.rsanches96@gmail.com>
2022-08-23 16:38:38 -0300

    Leitura de dados

--- curso_dev_kotlin/kotlin_basico_conceitos_iniciais/src/main/kotlin/Main.kt
+++ curso_dev_kotlin/kotlin_basico_conceitos_iniciais/src/main/kotlin/Main.kt
@@ -1,40 +1,6 @@
 import kotlin.math.*
 
 fun main() {
-    val bonus = bonusWhen("Gerente")
-    println(bonus)
-}
- fun calculaBonus(cargo: String, tempoDeExp: Int): Float{
-    if (cargo == "Gerente"){
-        if (tempoDeExp >= 2){
-            return 3000f
-        } else {
-            return 2000f
-        }
-    }
-    else if (cargo == "Coordenador"){
-        if (tempoDeExp >= 1){
-            return 1800f
-        } else {
-            return 1500f
-        }
-     }
-    else if (cargo == "Engenheiro de software"){
-         return 1000f
-     }
-    else if (cargo == "Estagiário"){
-         return 500f
-     }
-
-     return 0f
- }
-
-fun bonusWhen(cargo: String): Float{
-    return when (cargo){
-        "Gerente" -> 2000f
-        "Coordenador" -> 1500f
-        "Engenheiro de software" -> 1000f
-        "Estagiário" -> 500f
-        else -> 0f
-    }
+    val s: String? = readLine()
+    println(s)
 }
