f09c54d192dfb75f2469b94fa3a2cf1ac0253b27
Rafael R. Sanches <rafael.rsanches96@gmail.com>
2022-08-23 17:16:53 -0300

    Resolução - If/else

--- curso_dev_kotlin/kotlin_basico_conceitos_iniciais/src/main/kotlin/Main.kt
+++ curso_dev_kotlin/kotlin_basico_conceitos_iniciais/src/main/kotlin/Main.kt
@@ -1,6 +1,17 @@
 import kotlin.math.*
 
 fun main() {
-    val s: String? = readLine()
-    println(s)
+    println(ex1(5,6))
+    println(ex2(5,5,5))
+}
+
+fun ex1 (lado1: Int, lado2: Int): String {
+    if (lado1 == lado2) return "Forma um quadrado"
+    else return "Não forma um quadrado"
+}
+
+fun ex2 (lado1: Int, lado2: Int, lado3: Int): String{
+    if ((lado1 == lado2) && (lado1 == lado3)) return "Triângulo equilátero"
+    else return "Não é um triângulo equilátero"
+
 }
