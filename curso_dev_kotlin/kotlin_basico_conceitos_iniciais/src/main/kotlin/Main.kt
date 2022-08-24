bf0d28de554968f6be08db33e030bd0083714268
Rafael R. Sanches <rafael.rsanches96@gmail.com>
2022-08-24 10:55:46 -0300

    Resolução - while

--- curso_dev_kotlin/kotlin_basico_conceitos_iniciais/src/main/kotlin/Main.kt
+++ curso_dev_kotlin/kotlin_basico_conceitos_iniciais/src/main/kotlin/Main.kt
@@ -1,32 +1,57 @@
 import kotlin.math.*
 
 fun main() {
-    ex5(4)
+   ex2()
 }
 
-fun ex1() {
-    for (i in 1..50) print("$i ")
+fun ex1(){
+    var baloes: Int = 0
+
+    while (baloes * 7 + 7 < 2000){
+        baloes++
+    }
+    println("É necessário $baloes baloẽs para encher a caixa d'água de 2.000 litros.")
 }
 
 fun ex2(){
-    for (i in 50 downTo 1) print("$i ")
-}
-
-fun ex3(){
-    for (i in 1..50)
-        if (i % 5 != 0){
-            print("$i ")
+    var i: Int = 1
+    while (i <= 50){
+        if ((i % 3 == 0) && (i % 5 == 0)){
+            println("$i - FizzBuzz")
         }
-}
-
-fun ex4(){
-    var somaTotal: Int = 0
-    for (i in 1..500){
-        somaTotal += i;
+        else if (i % 3 == 0){
+            println("$i - Buzz")
+        }
+        else if (i % 5 == 0){
+            println("$i - Fizz")
+        } else{
+            println("$i")
+        }
+        i++
     }
-    print(somaTotal)
 }
 
-fun ex5(n: Int){
-    for (n in 1..n) println("#".repeat(n))
+fun ex3(str: String){
+    var contador: Int = str.length - 1
+    while (contador >= 0){
+        print(str[contador])
+        contador--
+    }
+}
+
+fun ex4(str: String): Boolean {
+    val strLower: String = str.lowercase()
+    var somaX: Int = 0
+    var somaO: Int = 0
+    var i: Int = 0
+    while (i < strLower.length){
+        if (strLower[i] == 'x'){
+            somaX++
+        }
+        else if (strLower[i] == 'o'){
+            somaO++
+        }
+        i++
+    }
+    return ((somaX == somaO) && (somaX != 0))
 }
