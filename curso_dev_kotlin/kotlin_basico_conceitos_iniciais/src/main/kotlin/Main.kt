922c1a35c16f5ed4f6a74115e22d19ad13bc6138
Rafael R. Sanches <rafael.rsanches96@gmail.com>
2022-08-24 08:36:39 -0300

    Controle de fluxo - for

--- curso_dev_kotlin/kotlin_basico_conceitos_iniciais/src/main/kotlin/Main.kt
+++ curso_dev_kotlin/kotlin_basico_conceitos_iniciais/src/main/kotlin/Main.kt
@@ -1,17 +1,17 @@
 import kotlin.math.*
 
 fun main() {
-    println(ex1(5,6))
-    println(ex2(5,5,5))
-}
+    for (i in 1..10){
+        print("$i ")
+    }
 
-fun ex1 (lado1: Int, lado2: Int): String {
-    if (lado1 == lado2) return "Forma um quadrado"
-    else return "Não forma um quadrado"
-}
+    for (j in 10 downTo 0){
+        print("$j ")
+    }
 
-fun ex2 (lado1: Int, lado2: Int, lado3: Int): String{
-    if ((lado1 == lado2) && (lado1 == lado3)) return "Triângulo equilátero"
-    else return "Não é um triângulo equilátero"
+    val str: String = "Kotlin é show!"
+    for (char in str){
+        print("$char ")
+    }
 
 }
