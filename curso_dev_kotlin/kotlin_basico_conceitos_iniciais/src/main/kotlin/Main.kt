39ef6917ebf35dc25c8ee4aeadd4d597ae6a5314
Rafael R. Sanches <rafael.rsanches96@gmail.com>
2022-08-24 11:41:54 -0300

    Exceções

--- curso_dev_kotlin/kotlin_basico_conceitos_iniciais/src/main/kotlin/Main.kt
+++ curso_dev_kotlin/kotlin_basico_conceitos_iniciais/src/main/kotlin/Main.kt
@@ -1,7 +1,21 @@
 import kotlin.math.*
 
 fun main() {
-    var s: String? = null
+    try {
+        println("Exemplo: tenta abrir banco")
 
-    println(s?.length)
+        val s: String? = null
+        println(s!!.length)
+
+        val a: Int = 10 / 0
+
+    } catch (e: NullPointerException) {
+        println("Variável nula!")
+    } catch (e: ArithmeticException){
+        println("Impossível dividir por zero!")
+    } catch (e: Exception){
+        println("Exceção genérica!")
+    } finally {
+        println("Exemplo: fecha banco")
+    }
 }
