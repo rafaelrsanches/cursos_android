ba89e04e17b0c8cf5647a79bfbba4ddc871d2d9a
Rafael R. Sanches <rafael.rsanches96@gmail.com>
2022-08-24 11:49:00 -0300

    Operador Elvis

--- curso_dev_kotlin/kotlin_basico_conceitos_iniciais/src/main/kotlin/Main.kt
+++ curso_dev_kotlin/kotlin_basico_conceitos_iniciais/src/main/kotlin/Main.kt
@@ -1,21 +1,7 @@
 import kotlin.math.*
 
 fun main() {
-    try {
-        println("Exemplo: tenta abrir banco")
+    val str: String? = null
 
-        val s: String? = null
-        println(s!!.length)
-
-        val a: Int = 10 / 0
-
-    } catch (e: NullPointerException) {
-        println("Variável nula!")
-    } catch (e: ArithmeticException){
-        println("Impossível dividir por zero!")
-    } catch (e: Exception){
-        println("Exceção genérica!")
-    } finally {
-        println("Exemplo: fecha banco")
-    }
+    println(str ?: "nulo")
 }
