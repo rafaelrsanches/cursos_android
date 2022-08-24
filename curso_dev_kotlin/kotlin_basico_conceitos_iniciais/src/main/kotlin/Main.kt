baf56cb93286ef976de5328566903ceec61a24c2
Rafael R. Sanches <rafael.rsanches96@gmail.com>
2022-08-24 11:15:08 -0300

    Resolução - Desafio

--- curso_dev_kotlin/kotlin_basico_conceitos_iniciais/src/main/kotlin/Main.kt
+++ curso_dev_kotlin/kotlin_basico_conceitos_iniciais/src/main/kotlin/Main.kt
@@ -1,57 +1,16 @@
 import kotlin.math.*
 
 fun main() {
-   ex2()
-}
+    val salario: Float = 10000f
+    var patAna: Float = 0f
+    var patPaula: Float = 0f
+    var qntdMeses: Int = 0
 
-fun ex1(){
-    var baloes: Int = 0
+    do {
+        patAna += (salario * 0.05f) + (salario * 0.05f) + (patAna * 0.002f)
+        patPaula += (salario * 0.05f) + (patPaula * 0.008f)
+        qntdMeses++
+    }while (patAna > patPaula)
 
-    while (baloes * 7 + 7 < 2000){
-        baloes++
-    }
-    println("É necessário $baloes baloẽs para encher a caixa d'água de 2.000 litros.")
-}
-
-fun ex2(){
-    var i: Int = 1
-    while (i <= 50){
-        if ((i % 3 == 0) && (i % 5 == 0)){
-            println("$i - FizzBuzz")
-        }
-        else if (i % 3 == 0){
-            println("$i - Buzz")
-        }
-        else if (i % 5 == 0){
-            println("$i - Fizz")
-        } else{
-            println("$i")
-        }
-        i++
-    }
-}
-
-fun ex3(str: String){
-    var contador: Int = str.length - 1
-    while (contador >= 0){
-        print(str[contador])
-        contador--
-    }
-}
-
-fun ex4(str: String): Boolean {
-    val strLower: String = str.lowercase()
-    var somaX: Int = 0
-    var somaO: Int = 0
-    var i: Int = 0
-    while (i < strLower.length){
-        if (strLower[i] == 'x'){
-            somaX++
-        }
-        else if (strLower[i] == 'o'){
-            somaO++
-        }
-        i++
-    }
-    return ((somaX == somaO) && (somaX != 0))
+    println("Paula vai passar o patrimônio de Ana no mês $qntdMeses")
 }
