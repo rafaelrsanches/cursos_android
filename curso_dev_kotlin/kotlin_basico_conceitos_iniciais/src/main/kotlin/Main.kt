5878e2418ea43e9bc96cfe8045cf37116dd90fbb
Rafael R. Sanches <rafael.rsanches96@gmail.com>
2022-08-24 11:27:26 -0300

    Null safety

--- curso_dev_kotlin/kotlin_basico_conceitos_iniciais/src/main/kotlin/Main.kt
+++ curso_dev_kotlin/kotlin_basico_conceitos_iniciais/src/main/kotlin/Main.kt
@@ -1,16 +1,7 @@
 import kotlin.math.*
 
 fun main() {
-    val salario: Float = 10000f
-    var patAna: Float = 0f
-    var patPaula: Float = 0f
-    var qntdMeses: Int = 0
+    var s: String? = null
 
-    do {
-        patAna += (salario * 0.05f) + (salario * 0.05f) + (patAna * 0.002f)
-        patPaula += (salario * 0.05f) + (patPaula * 0.008f)
-        qntdMeses++
-    }while (patAna > patPaula)
-
-    println("Paula vai passar o patrimônio de Ana no mês $qntdMeses")
+    println(s?.length)
 }
