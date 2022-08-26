c41e10043163e75eec640e21f2584238b64f7961
Rafael R. Sanches <rafael.rsanches96@gmail.com>
2022-08-26 10:50:03 -0300

    Construtores primários e secundários

--- curso_dev_kotlin/OrientacaoObjetos/src/main/kotlin/main.kt
+++ curso_dev_kotlin/OrientacaoObjetos/src/main/kotlin/main.kt
@@ -2,17 +2,20 @@ fun main(){
     // class - comportamentos e atributos
 
     // classe (instancia) objetos || "... = Classe()" == instancia, gera o objeto
-    var pessoa: Pessoa = Pessoa(2014, "Monero")
+    var pessoa: Pessoa = Pessoa(2014, "Monero", "2014X2014X")
 
     pessoa.nome
     pessoa.acordar()
-    pessoa.olhos
 
 }
 
 class Pessoa(val anoNascimento: Int, var nome: String){
 
-    var olhos: String = ""
+    var documento: String? = null
+
+    constructor(anoNascimento: Int, nome: String, doc: String) : this(anoNascimento, nome){
+        this.documento= doc
+    }
 
     fun dormir(){
 
