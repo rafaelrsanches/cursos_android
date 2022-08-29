db4fc7c95884123f0d3ebf0e5ba39bf95342e7c3
Rafael R. Sanches <rafael.rsanches96@gmail.com>
2022-08-29 10:09:14 -0300

    Modificadores de visibilidade

--- curso_dev_kotlin/heranca/src/main/kotlin/main.kt
+++ curso_dev_kotlin/heranca/src/main/kotlin/main.kt
@@ -7,11 +7,22 @@ fun main() {
 }
 
 open class Eletronico(var marca: String){
-    fun ligar(){}
-    fun desligar(){}
+
+    private val s: String = ""
+
+    private fun ativarCorrenteEletrica() {}
+    fun ligar() {
+        ativarCorrenteEletrica()
+    }
+    fun desligar() {}
 }
 
 class Computador(marca: String) : Eletronico(marca){
     fun instalarSoftware() {}
     fun processarDados() {}
 }
+
+private class X // private class só permite acesso no mesmo arquivo e também impede que seja herdada.
+
+// private fun dentro de uma classe, só a própria classe enxerga.
+// protected fun dentro de uma classe, a própria classe e as classes herdeiras enxergam.
