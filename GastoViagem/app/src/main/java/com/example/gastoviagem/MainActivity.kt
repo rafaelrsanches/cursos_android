20f30e08bf7a758124952feb0579274e564170a6
Rafael R. Sanches <rafael.rsanches96@gmail.com>
2022-09-06 16:03:21 -0300

    Evento de click

--- GastoViagem/app/src/main/java/com/example/gastoviagem/MainActivity.kt
+++ GastoViagem/app/src/main/java/com/example/gastoviagem/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.gastoviagem
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.gastoviagem.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityMainBinding
 
@@ -13,5 +15,18 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.buttonCalculate.setOnClickListener(this)
+
     }
+
+    override fun onClick(view: View){
+        if (view.id == R.id.button_calculate){
+            calculate()
+        }
+    }
+
+    private fun calculate(){
+        Toast.makeText(this, "Fui clicado!", Toast.LENGTH_SHORT).show()
+    }
+
 }
