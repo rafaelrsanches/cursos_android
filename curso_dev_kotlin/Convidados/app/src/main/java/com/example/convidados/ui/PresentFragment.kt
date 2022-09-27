efff2934355ee8a9f2c2f547ab3fa46f91a7ab39
Rafael R. Sanches <rafael.rsanches96@gmail.com>
2022-09-27 15:15:23 -0300

    Modificações iniciais

--- curso_dev_kotlin/Convidados/app/src/main/java/com/example/convidados/ui/PresentFragment.kt
+++ curso_dev_kotlin/Convidados/app/src/main/java/com/example/convidados/ui/PresentFragment.kt
@@ -1,4 +1,4 @@
-package com.example.convidados.ui.gallery
+package com.example.convidados.ui
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.convidados.databinding.FragmentGalleryBinding
+import com.example.convidados.databinding.FragmentPresentBinding
 
-class GalleryFragment : Fragment() {
+class PresentFragment : Fragment() {
 
-    private var _binding: FragmentGalleryBinding? = null
+    private var _binding: FragmentPresentBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class GalleryFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val galleryViewModel =
-            ViewModelProvider(this).get(GalleryViewModel::class.java)
+        val viewModel =
+            ViewModelProvider(this).get(PresentViewModel::class.java)
 
-        _binding = FragmentGalleryBinding.inflate(inflater, container, false)
+        _binding = FragmentPresentBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textGallery
-        galleryViewModel.text.observe(viewLifecycleOwner) {
+        viewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
