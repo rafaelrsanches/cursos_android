f0587ca13934e0ceacc23773472f388aaeca1bb8
Rafael R. Sanches <rafael.rsanches96@gmail.com>
2022-09-27 15:55:09 -0300

    Cadastro - Layout, ViewModel e Eventos

--- curso_dev_kotlin/Convidados/app/src/main/java/com/example/convidados/GuestFormActivity.kt
+++ curso_dev_kotlin/Convidados/app/src/main/java/com/example/convidados/GuestFormActivity.kt
@@ -2,10 +2,33 @@ package com.example.convidados
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.ViewModelProvider
+import com.example.convidados.databinding.ActivityGuestFormBinding
 
-class GuestFormActivity : AppCompatActivity() {
+class GuestFormActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityGuestFormBinding
+    private lateinit var viewModel: GuestFormViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_guest_form)
+
+        binding = ActivityGuestFormBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        viewModel = ViewModelProvider(this).get(GuestFormViewModel::class.java)
+
+        binding.radioPresent.isChecked = true
+
+        // Eventos
+        binding.buttonSave.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        if (view.id == R.id.button_save){
+
+        }
     }
 }
---
+++ curso_dev_kotlin/Convidados/app/src/main/java/com/example/convidados/GuestFormViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.convidados
+
+import androidx.lifecycle.ViewModel
+
+class GuestFormViewModel: ViewModel() {
+}
